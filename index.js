9085eecd994575754bbb2fc31d85f6b5fd63766d
iamssh <saadhasan620@gmail.com>
2020-05-09 16:08:00 +0530

    complete without styling

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const app = require('express')(),
+const express = require('express'),
+    app = express(),
     bodyParser = require('body-parser'),
     cheerio = require('cheerio'),
     site = require('./site'),
-    osmosis = require('osmosis')
-    ;
+    axios = require('axios');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// app.use(__dirname, express.static('public'))
 
 // Globals
 let title,
     description,
     learning,
     image,
+    expandMessage,
     content;
 
 
@@ -26,63 +28,39 @@ app.get('/search', (req, res) => {
 });
 
 app.post('/submit', (req, res) => {
-    // Perform scraping
 
-    const $ = cheerio.load(site);
-    title = $('h1.clp-lead__title').html();
-    learning = $('.what-you-get__items')
-                .find('.what-you-get__text')
-                .contents().toArray()
-    description = $('div.clp-component-render div.description div.js-simple-collapse-inner').html()
-    // console.log(
-    //     $('.ud-component--clp--curriculum').text()
-    // );
-
-
-    // function fun() {
-    //     return new Promise((resolve, reject) => {
-    //         let result;
-
-    //         osmosis.get('http://localhost:8080/samplePage2')
-    //             .set({title:'h1'})
-    //             .data(res=>{result = res})
-    //             .error(err => reject(err))
-    //             .done(() => resolve(result))
-    //     })
-    // }
-
-    // fun().then(console.log).catch(console.log);
-
-
-    // axios.get('https://www.udemy.com/course/complete-python-bootcamp/')
-    //     .then(function (response) {
-    //         // handle success
-    //         
-    //         
-    //     })
-    //     .catch(function (error) {
-    //         // handle error
-    //         console.log(error);
-    //     })
-    //     .then(function () {
-    //         // always executed
-    //     });
-
-    // render new page with scraped details
-    res.render("result", {learning:learning, title:title})
-    // res.send("results")
+    axios.get('https://www.udemy.com/course/practical-leadership/')
+        .then(function (response) {
+            // handle success
+            const $ = cheerio.load(response.data);
+            title = $('h1.clp-lead__title').html();
+            learning = $('.what-you-get__items')
+                        .find('.what-you-get__text')
+                        .contents().toArray()
+            description = $('div.clp-component-render div.description div.js-simple-collapse-inner').html();
+            image = $('.introduction-asset img').attr('src')
+            content = $('.curriculum-wrapper')
+                        .find('.section-title-text')
+                        .contents()
+                        .toArray()
+            expandMessage = $('.section-container--more-sections').find('span').html()
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+            res.render("result", 
+                {
+                    expandMessage:expandMessage,
+                    description: description, 
+                    learning:learning, 
+                    content: content, 
+                    title:title,
+                    image: image
+                })
+        });
 });
-
-app.get('/samplePage', (req, res) => {
-    res.render('samplePage');
-})
-
-app.get('/samplePage2', (req, res) => {
-    res.render('samplePage2');
-})
-
-app.get('/cloneSite', (req, res) => {
-    res.render('cloneSite');
-})
 
 app.listen(8080, () => console.log("Started..."));
