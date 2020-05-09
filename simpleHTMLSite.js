d72c7a8d3365c6d76519f2a3aff34d47bda304fd
iamssh <saadhasan620@gmail.com>
2020-05-09 11:24:58 +0530

    replace cheerio with osmosis

---
+++ simpleHTMLSite.js
@@ -0,0 +1,23 @@
+module.exports = 
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sampl2</title>
+</head>
+<body>
+    <h1>
+        Sample 2
+    </h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, eaque?</p>
+    <ul>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+    </ul>
+</body>
+</html>
+`
